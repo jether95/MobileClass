183ef07de8c6e7a5b6d6a0302b109489b864fe58
jether95 <juaco111395@gmail.com>
2021-03-05 09:47:17 -0500

    Pantalla Login de instagram 90 porciento terminada

---
+++ Clase1/app/src/main/java/com/jet/ejemploclase1/InstagramActivity.java
@@ -0,0 +1,14 @@
+package com.jet.ejemploclase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstagramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instagram);
+    }
+}
--- Clase1/app/src/main/java/com/jet/ejemploclase1/MainActivity.java
+++ Clase1/app/src/main/java/com/jet/ejemploclase1/MainActivity.java
@@ -3,12 +3,44 @@ package com.jet.ejemploclase1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etLoginName;
+    private EditText etLoginPassword;
+    private Button btnLoginSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView();
+        btnLoginSend.setOnClickListener(this);
+        etLoginName.setOnClickListener(this);
+        etLoginPassword.setOnClickListener(this);
+
+    }
+
+    private void setContentView(){
+        etLoginName = findViewById(R.id.txtUsername);
+        etLoginPassword = findViewById(R.id.txtPassword);
+        btnLoginSend = findViewById(R.id.btnLogin);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnLogin){
+            sendLogin();
+        }
+    }
+
+    private void sendLogin() {
+        String fullText = etLoginName.getText().toString();
+        Toast.makeText(this,"hola"+ fullText,Toast.LENGTH_LONG).show();
     }
 }
